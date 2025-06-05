5ed54a03eb8dfad574da5d48a9c0f3712e5428b4
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-06-05 15:47:18 +0530

    refactor: add code comments explaining functions

--- incliarray.h
+++ incliarray.h
@@ -16,12 +16,20 @@ public:
   int size = 0;
   bool ownsData;
 
+  // To create an object (for general usage)
   NDArray(std::vector<int> shape);
+
+  // To display the metadata information of the object
   void metadata(bool shapeInfo = true, bool stridesInfo = false,
                 bool ndimInfo = false, bool sizeInfo = false,
                 bool ownsDataInfo = false);
 
+  // To get an element based on the indices
   float get(std::vector<int> indices);
+
+  // To set an element based on the indices
   void set(float value, std::vector<int> indices);
+
+  // To get a sliced object
   NDArray slice(std::vector<std::tuple<int, int>> indices);
 };
